22a5471ba0339a017be11e382dedff51775b0ad9
ebenezet <ebenezer.t@hotmail.fr>
2025-07-20 00:47:52 +0100

    managed to display comments for each posts

--- src/component/Comments.js
+++ src/component/Comments.js
@@ -4,23 +4,19 @@ import { db } from "@/utils/utilities";
 export default async function CommentsPage({ params }) {
   const { id } = await params;
 
-  const reviews = (
-    await db.query(
-      `SELECT  films.title, (
-  SELECT ARRAY_AGG(comment)   FROM reviews
-  WHERE reviews.film_id = films.id
-)       as reviews
-FROM films `
-    )
-  ).rows[0];
+  const reviews = (await db.query(`SELECT * FROM reviews WHERE id = $1`, [id]))
+    .rows;
 
   console.log(reviews);
   return (
     <div>
       <h1>The film reviews</h1>
       <ul>
-        {reviews.reviews.map((review, index) => (
-          <li key={index}>{review.name}</li>
+        {reviews.map((review) => (
+          <div key={review.id}>
+            <li>{review.name}</li>
+            <li>{review.comment}</li>
+          </div>
         ))}
       </ul>
     </div>
